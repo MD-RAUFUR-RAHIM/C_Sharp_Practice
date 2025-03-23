774f09d4af65f7f9af8c4fb41f221045cfdc081e
Md Raufur Rahim <raufurrahim2000@gmail.com>
2025-03-23 23:50:53 +0600

    Extension Method

---
+++ DelegateExample/ExtensionMethod/PersonExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethod
+{
+    public static class PersonExtension
+    {
+        public static string GetInfo(this Person person)
+        {
+            return "Id: " + person.Id + " Name: " + person.Name + "Address: " + person.Address;
+        }
+    }
+}
---
+++ DelegateExample/ExtensionMethod/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExtensionMethod;
+
+Console.WriteLine("Hello, World!");
+Person person = new Person();
+person.Id = 101;
+person.Name = "Maruf";
+person.Address = "Tangail";
+string info = person.GetInfo();
+Console.WriteLine(info);
+Console.ReadKey();  
