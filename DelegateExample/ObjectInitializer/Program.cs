abc8aa0d2b8be61600b2223e58918eb09a6c64a1
Md Raufur Rahim <raufurrahim2000@gmail.com>
2025-03-24 08:22:43 +0600

    Object & Collection Initializer

---
+++ DelegateExample/ObjectInitializer/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ObjectInitializer;
+
+Console.WriteLine("Hello, World!");
+Student student = new Student()
+{
+    Id = 1,
+    Name = "Maruf"
+};
