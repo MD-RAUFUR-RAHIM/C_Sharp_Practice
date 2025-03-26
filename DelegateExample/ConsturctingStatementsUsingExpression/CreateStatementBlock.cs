f5edf8afb2a5a8e7fd0c317a93d6a33498eef74b
Md Raufur Rahim <raufurrahim2000@gmail.com>
2025-03-27 01:43:06 +0600

    ConsturctingStatementsUsingExpression

---
+++ DelegateExample/ConsturctingStatementsUsingExpression/CreateStatementBlock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsturctingStatementsUsingExpression
+{
+    public class CreateStatementBlock
+    {
+        public Expression StateMentBlock()
+        { 
+            var consoleWriteMethod = typeof(Console)
+                .GetMethod(nameof(Console.Write), new Type[] {typeof(string) });
+            
+            var consoleWriteLineMethod = typeof(Console)
+                .GetMethod(nameof(Console.WriteLine), new Type[] { typeof(string) });
+
+            return Expression.Block(
+                Expression.Call(consoleWriteMethod, Expression.Constant("Hello ")),
+                Expression.Call(consoleWriteLineMethod, Expression.Constant("World!"))
+                );
+        }
+    }
+}
