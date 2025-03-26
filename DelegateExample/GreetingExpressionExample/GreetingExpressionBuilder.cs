39332c03802f6791f682dc11e8da1eec6c956e5a
Md Raufur Rahim <raufurrahim2000@gmail.com>
2025-03-26 17:08:50 +0600

    Add GreetingExpression with dynamic greeting message using Expression Trees

---
+++ DelegateExample/GreetingExpressionExample/GreetingExpressionBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreetingExpressionExample
+{
+    public class GreetingExpressionBuilder
+    {
+        public Func<string, string?> ConcatGreetingExpression()
+        {
+            var personParameter = Expression.Parameter(typeof(string), "personName");
+            var isNullOrWhiteSpaceMethod = typeof(string).GetMethod(nameof(string.IsNullOrWhiteSpace));
+
+            var condition = Expression.Not(
+                Expression.Call(
+                    isNullOrWhiteSpaceMethod, personParameter));
+
+            //var trueClause = Expression.Add(
+            //    Expression.Constant("Greetings, "), personParameter
+            //    );
+
+            var trueClause = Expression.Call(
+                typeof(string).GetMethod("Concat", new Type[] { typeof (string), typeof(string) }),
+                Expression.Constant("Greetings, "),
+                personParameter
+                );
+
+            var falseClause = Expression.Constant(null, typeof(string));
+
+            var conditional = Expression.Condition(condition, trueClause, falseClause);
+
+            var lambda = Expression.Lambda<Func<string, string?>>(conditional, personParameter);
+            return lambda.Compile();
+
+        }
+    }
+}
---
+++ DelegateExample/GreetingExpressionExample2/Program.cs
@@ -0,0 +1,12 @@
+﻿using GreetingExpressionExample2;
+using System.Linq.Expressions;
+
+var greetingExpression = new GreetingExpression();
+
+// Compile the lambda directly
+var greetingsFunc = greetingExpression.Greetings().Compile();
+
+// Test the greeting function with different inputs
+Console.WriteLine(greetingsFunc("Alice")); // Should print: Greetings, Alice
+Console.WriteLine(greetingsFunc("")); // Should print: (null)
+Console.WriteLine(greetingsFunc(null)); // Should print: (null)
