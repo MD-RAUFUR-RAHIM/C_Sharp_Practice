a4f20eafca9496cf2853fcd5266a602abb818578
Md Raufur Rahim <raufurrahim2000@gmail.com>
2025-03-30 02:58:42 +0600

    GenericOperatorsUsingExpression

---
+++ DelegateExample/GenericOperatorsUsingExpression/OptimizedCalculate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GenericOperatorsUsingExpression
+{
+    public static class OptimizedCalculate<T>
+    {
+        public static Func<T, T> Of { get; } = CreateFunction();
+        private static Func<T, T> CreateFunction()
+        {
+            var param = Expression.Parameter(typeof(T));
+            var three = Expression.Convert(Expression.Constant(3), typeof(T));
+            var four = Expression.Convert(Expression.Constant(4), typeof(T));
+            var operation = Expression.Divide(Expression.Multiply(param, three), four);
+            return Expression.Lambda<Func<T, T>>(operation, param).Compile();
+        }
+        public static int x = y;
+        private static int y=7;
+    }
+}
---
+++ DelegateExample/OptimizingReflection/ReflectionCached.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptimizingReflection
+{
+    public class ReflectionCached
+    {
+        private static MethodInfo ExecuteMethod { get; } = typeof(Command)
+            .GetMethod("Execute", BindingFlags.NonPublic | BindingFlags.Instance);
+        public static int CallExecute(Command command1) => 
+            (int) ExecuteMethod.Invoke(command1, null);
+    }
+}
