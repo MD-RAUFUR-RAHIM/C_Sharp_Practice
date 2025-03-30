eaa1097833f8f1b7d568739a88dde5306cb9dcdf
Md Raufur Rahim <raufurrahim2000@gmail.com>
2025-03-30 22:05:17 +0600

    FirstOrDefaultExample GroupByExample LINQExamples

---
+++ LINQExamples/GroupByExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using GroupByExample;
+
+List<Person> people =  new List<Person>()
+{
+            new Person { Name = "Alice", Age = 25 },
+            new Person { Name = "Bob", Age = 30 },
+            new Person { Name = "Charlie", Age = 25 },
+            new Person { Name = "David", Age = 35 },
+            new Person { Name = "Eve", Age = 30 }
+};
+var groupByAge = people.GroupBy(person => person.Age);
+foreach(var group in  groupByAge)
+{
+    Console.WriteLine($"Age Group: {group.Key}");
+    foreach (var person in group)
+    {
+        Console.WriteLine($"  {person.Name}");
+    }
+}
