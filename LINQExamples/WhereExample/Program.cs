6ac9ab7cb4983b929eff650ad9157f4ecc9d113c
Md Raufur Rahim <raufurrahim2000@gmail.com>
2025-03-30 23:09:42 +0600

    Sorting Data using OrderBy and OrderByDescending

--- LINQExamples/WhereExample/Program.cs
+++ LINQExamples/WhereExample/Program.cs
@@ -23,3 +23,28 @@ foreach(var p in person)
 {
     Console.WriteLine($"{p.Name} {p.Age} years old");
 }
+
+List<Person> people1 = new List<Person>
+        {
+            new Person { Name = "Alice", Age = 30 },
+            new Person { Name = "Bob", Age = 25 },
+            new Person { Name = "Charlie", Age = 30 },
+            new Person { Name = "David", Age = 35 },
+            new Person { Name = "Eve", Age = 25 },
+            new Person { Name = "Aeon", Age = 30 }
+        };
+var orderByAscending = people1.OrderBy(p => p.Age).ThenBy(p => p.Name);
+Console.WriteLine();
+Console.WriteLine("Sorting By Age and Name in Ascending order");
+foreach(var p in orderByAscending)
+{
+    Console.WriteLine($"{p.Name} {p.Age} years old");
+}
+
+Console.WriteLine();
+Console.WriteLine("Sorting By Age and Name in Descending order");
+var orderbyDescending = people1.OrderByDescending(p => p.Age).ThenByDescending(p => p.Name);
+foreach (var p in orderbyDescending)
+{
+    Console.WriteLine($"{p.Name} {p.Age} years old");
+}
